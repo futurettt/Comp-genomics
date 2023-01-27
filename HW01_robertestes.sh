c9325c83ccc85b0a1d73a6fcc641ca0fad729d7e
futurettt <123781312+futurettt@users.noreply.github.com>
2023-01-27 18:57:41 -0500

    Update HW01_robertestes.sh

--- HW01_robertestes.sh
+++ HW01_robertestes.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 cd /blue/mcb4934/share/robert.estes
-mkdir Homework01 
+mkdir Homework01
 cd Homework01
-pwd
 wget https://download.maizegdb.org/Zm-B73-REFERENCE-NAM-5.0/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3.gz
 wget https://download.maizegdb.org/Zm-B73-REFERENCE-NAM-5.0/Zm-B73-REFERENCE-NAM-5.0.fa.gz
 gunzip Zm-B73-REFERENCE-NAM-5.0.fa.gz
@@ -13,4 +11,3 @@ grep -w -c "gene" Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3
 grep -w -c "exon" Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3
 grep "exon" Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 > Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3_exon 
 cut -f 1,3,4,5,7,9 Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3_exon > Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3_exon_part
-
